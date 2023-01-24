4c2036cae9d185ae7bffd7e68b822b77098b5cbb
levyhb <levybholanda@gmail.com>
2023-01-24 14:58:05 -0300

    feat: login validations

--- app/backend/src/routes/login.routes.ts
+++ app/backend/src/routes/login.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import loginValidation from '../middleware/login.validation';
 import loginController from '../controller/login.controller';
 
 const router = Router();
 
-router.post('/', loginController);
+router.post('/', loginValidation, loginController);
 
 export default router;
