314588a5a373b1268676adacd5a829840c424100
levyhb <levybholanda@gmail.com>
2023-01-24 22:33:59 -0300

    feat: endpoint get /teams/:id

--- app/backend/src/controller/teams.controller.ts
+++ app/backend/src/controller/teams.controller.ts
@@ -6,6 +6,18 @@ const getAllTeams = async (req: Request, res: Response) => {
   return res.status(200).json(allTeams);
 };
 
+const getTeamById = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+    const team = await teamsService.getTeamById(id);
+    return res.status(200).json(team);
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
 export default {
   getAllTeams,
+  getTeamById,
 };
--- app/backend/src/services/teams.service.ts
+++ app/backend/src/services/teams.service.ts
@@ -2,6 +2,9 @@ import Team from '../database/models/Teams.model';
 
 const getAllTeams = async () => Team.findAll();
 
+const getTeamById = async (id:number) => Team.findOne({ where: { id } });
+
 export default {
   getAllTeams,
+  getTeamById,
 };
